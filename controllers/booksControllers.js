c33504bb754983ec507266c642c8d7e48b9c27f3
Deepali Garg <deepaligarg.chd@gmail.com>
2021-05-14 23:33:22 -0400

    file structure changes

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function(req, res) {
+findAll: function(req, res) {
     db.Book
       .find(req.query)
       .sort({ date: -1 })
